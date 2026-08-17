22787c418fef0289d3ad8920d1df4d65afe535f8
Jeandre Visagie <st10506626@rcconnect.edu.za>
2026-08-17 11:02:05 +0200

    inpatient class done

---
+++ Inpatient.java
@@ -0,0 +1,18 @@
+public class Inpatient extends Patient {
+    private String wardNumber;
+    private String bedNumber;
+    
+    public Inpatient(String patientID, String firstName, String lastName, int age, String gender, String medicalCondition, String wardNumber) {
+        super(patientID, firstName, lastName, age, gender, medicalCondition, PatientCategory.INPATIENT);//calling parent class using super
+        this.wardNumber = wardNumber;
+        this.bedNumber = "Not Assigned";
+    }
+
+    public String getBedNumber() { return bedNumber; }
+    public void setBedNumber(String bedNumber) { this.bedNumber = bedNumber; }
+
+    @Override
+    public String displayDetails() {
+        return super.displayDetails() + String.format(" | Ward: %s | Bed: %s", wardNumber, bedNumber);
+    }
+}
